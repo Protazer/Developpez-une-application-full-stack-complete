bf94ecf7afd00dba22c90cb94aa0bff66dda4ebe
florian <florian.huberdeau@laposte.net>
2025-07-02 17:25:31 +0200

    feat(front/back): Update project version

--- back/src/main/java/com/openclassrooms/mdd_api/MddApiApplication.java
+++ back/src/main/java/com/openclassrooms/mdd_api/MddApiApplication.java
@@ -1,4 +1,4 @@
-package com.openclassrooms.mddapi;
+package com.openclassrooms.mdd_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
