bae4884bd72e323119b5cd4213eab8e77f868f09
florian <florian.huberdeau@laposte.net>
2025-08-04 10:39:56 +0200

    feat/refactor(#6): Add front profile page and update usrer form. Refactor error message from back into forms.

--- front/src/app/pages/profile/profile.component.ts
+++ front/src/app/pages/profile/profile.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProfileFormComponent} from '../../core/components/profile-form/profile-form.component';
+import {IUser} from '../../interfaces/user.interface';
+import {SessionService} from '../../core/services/session.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [
+    ProfileFormComponent
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  public user!: IUser;
 
+  constructor(private sessionService: SessionService, private router: Router) {
+  }
+
+  ngOnInit() {
+    const user = this.sessionService.user;
+    if (!user) {
+      this.router.navigateByUrl('/login');
+    } else {
+      this.user = user;
+    }
+  }
 }
