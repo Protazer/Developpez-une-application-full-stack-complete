7a230419399dc2000ebb2e8fa06955b4fb7160b3
florian <florian.huberdeau@laposte.net>
2025-08-04 10:43:57 +0200

    feat/refactor(#6): Add backend user update service,controller and refactor

--- back/src/main/java/com/openclassrooms/mdd_api/service/IUserService.java
+++ back/src/main/java/com/openclassrooms/mdd_api/service/IUserService.java
@@ -3,6 +3,7 @@ package com.openclassrooms.mdd_api.service;
 import com.openclassrooms.mdd_api.model.User;
 import com.openclassrooms.mdd_api.payload.request.UserLoginRequestDto;
 import com.openclassrooms.mdd_api.payload.request.UserRegisterDto;
+import com.openclassrooms.mdd_api.payload.request.UserUpdateRequestDto;
 import com.openclassrooms.mdd_api.payload.response.GetUserResponseDto;
 import com.openclassrooms.mdd_api.payload.response.UserAuthResponseDto;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
@@ -21,4 +22,6 @@ public interface IUserService {
 	UserAuthResponseDto loginUser(final UserLoginRequestDto user);
 
 	GetUserResponseDto getUser(final JwtAuthenticationToken token);
+
+	GetUserResponseDto updateUser(final JwtAuthenticationToken token, final UserUpdateRequestDto user);
 }
